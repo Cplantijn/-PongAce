09a68c09a0f778ef8525a32b96955cc1fd2178ee
Christopher Plantijn <chris@simeoncreative.com>
2015-12-05 22:10:58 -0500

    Update to views rendered

---
+++ src/js/components/CardBottom.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react'
+import classNames from 'classnames'
+
+
+export default class CardBottom extends Component {
+  constructor(props) {
+    super(props)
+  }
+  render() {
+    return (
+      <div className="team-card-bottom-component">
+      </div>
+    )
+  }
+}
--- src/js/components/MainComponent.js
+++ src/js/components/MainComponent.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react'
+import _ from 'underscore'
 import Footer from './Footer'
 import TopBar from './TopBar'
 import TeamCard from './TeamCard'
@@ -8,12 +9,26 @@ export default class MainComponent extends Component {
     super(props)
   }
   render() {
+    var { teamData, addTeam } = this.props.props;
+    var teamCardOne, teamCardTwo;
+
+      if (typeof teamData[0] !== 'undefined') {
+        teamCardOne = <TeamCard active={true} score={teamData[0].score} cardType="team-1"/>
+      } else {
+        teamCardOne = <TeamCard addTeam={addTeam} cardType="team-1" active={false} />
+      }
+
+      if (typeof teamData[1] !== 'undefined') {
+        teamCardTwo = <TeamCard active={true} score={teamData[1].score} cardType="team-2"/>
+      } else {
+        teamCardTwo = <TeamCard addTeam={addTeam} cardType="team-2" active={false} />
+      }
     return (
       <div className="main-component container-fluid">
         <TopBar />
         <div className="card-component-container">
-          <TeamCard cardType="team-1"/>
-          <TeamCard cardType="team-2"/>
+          {teamCardOne}
+          {teamCardTwo}
         </div>
         <Footer />
       </div>
