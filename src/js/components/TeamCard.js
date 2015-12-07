50930734bb6b64202943ea6fbe54c54c35af9112
Christopher Plantijn <chris@simeoncreative.com>
2015-12-06 23:48:45 -0500

    Seperate card Objects, Add Modals

--- src/js/components/TeamCard.js
+++ src/js/components/TeamCard.js
@@ -2,36 +2,36 @@ import React, { Component } from 'react'
 import classNames from 'classnames'
 import Score from './Score'
 import CardBottom from './CardBottom'
-import TeamJoin from './TeamJoin'
+import EmptyCard from './EmptyCard'
+import _ from 'underscore'
 
 export default class TeamCard extends Component {
   constructor(props) {
     super(props)
   }
   render() {
-    console.log(this.props);
-    var cardContent;
-    var teamIndex;
+    var { cardData, teamIndex } = this.props,
+        teamPresent = _.size(cardData[teamIndex]),
+        cardBody
+
     var divClass = classNames({
       'team-card-component': true,
-      'team-1': this.props.cardType == 'team-1',
-      'team-2': this.props.cardType == 'team-2',
-      'inactive': !this.props.active
-    });
-    if (this.props.cardType == 'team-1') {
-      teamIndex = 0;
-    } else if (this.props.cardType == 'team-2') {
-      teamIndex = 1;
-    }
-    if (this.props.active) {
-      cardContent = <Score isServing={this.props.isServing} score={this.props.score}/>
+      'inactive': !teamPresent,
+      'active': teamPresent
+    })
+    if (teamPresent) {
+        cardBody = <div>
+                      <Score
+                       score={cardData} />
+                      <CardBottom />
+                  </div>
     } else {
-      cardContent = <TeamJoin teamIndex={teamIndex} team={this.props.cardType} addTeam={this.props.addTeam}/>
+        cardBody = <EmptyCard teamIndex={teamIndex} />
     }
+
     return (
       <div className={divClass}>
-        {cardContent}
-        <CardBottom / >
+        {cardBody}
       </div>
     )
   }
