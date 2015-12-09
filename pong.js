7a538cb3269e9f41529ece8f1cc3d9a76eddd2f5
Christopher Plantijn <chris@simeoncreative.com>
2015-12-09 01:43:27 -0500

    Create SQLite database and connections.

--- pong.js
+++ pong.js
@@ -3,6 +3,9 @@ var express   = require('express');
 var app       = express();
 var http      = require('http').Server(app);
 var io        = require('socket.io')(http);
+var config    = require('./config');
+var sqlite3   = require('sqlite3').verbose();
+var db        = new sqlite3.Database(config.dbFile);
 var five      = require('johnny-five');
 var board, buttonOne, buttonTwo;
 
@@ -21,8 +24,8 @@ io.on('connection', function() {
 board = new five.Board();
 board.on('ready', function() {
 
-  buttonOne = new five.Button(2);
-  buttonTwo = new five.Button(3);
+  buttonOne = new five.Button(config.buttonOnePin);
+  buttonTwo = new five.Button(config.buttonTwoPin);
 
   buttonOne.on('down', function() {
     io.emit('btnPress', 'Button One has been pressed!');
@@ -40,5 +43,34 @@ board.on('ready', function() {
     io.emit('btnPress', 'Button Two has been pressed!');
     console.log('btn held')
   });
-
 });
+
+function createProfileTable() {
+  db.serialize(function() {
+    db.run("CREATE TABLE profile ( \
+             id INTEGER PRIMARY KEY, \
+             first_name TEXT, \
+             last_name TEXT, \
+             image_name TEXT, \
+             solo_wins INTEGER DEFAULT 0, \
+             solo_losses INTEGER DEFAULT 0, \
+             doubles_wins INTEGER DEFAULT 0, \
+             doubles_losses INTEGER DEFAULT 0)");
+  });
+  console.log('profile table created!')
+  db.close();
+}
+
+function createGameHistoryTable() {
+  db.serialize(function() {
+    db.run("CREATE TABLE history (\
+              id INTEGER PRIMARY KEY, \
+              team_one_player_id TEXT, \
+              team_two_player_id TEXT, \
+              team_one_point INTEGER, \
+              team_two_point INTEGER, \
+              game_type TEXT)");
+  });
+  console.log('history table created!')
+  db.close();
+}
